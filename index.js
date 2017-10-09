8529ec220c9551fc32ae576f412cdb907d6d79ab
stopvoldemort <david.squires1@gmail.com>
2017-10-09 11:32:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let i=0; i<array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  const flips = []
+  while (Math.random() >= 0.5) {
+    flips.push("tails")
+  }
+  return `You got ${flips.length} tails in a row!`
+}
